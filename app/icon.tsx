37fd35b32f5c2ad73936b41e9d96aabd24ead75f
aoao84951-png <aoao84951@gmail.com>
2026-08-20 00:20:11 +0900

    feat: update app icon typography

--- app/icon.tsx
+++ app/icon.tsx
@@ -17,24 +17,43 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#ffffff",
+          background: "#0f2a5f",
         }}
       >
-        <svg
-          width="300"
-          height="300"
-          viewBox="0 0 320 320"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
+        <div
+          style={{
+            width: "420px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+            color: "#f2b8c6",
+            fontFamily: "Arial, Helvetica, sans-serif",
+            fontWeight: 900,
+            letterSpacing: "-8px",
+            marginLeft: "46px",
+            marginTop: "12px",
+          }}
         >
-          <path
-            d="M38 93L78 53L170 83L225 28L263 35L272 73L217 128L247 242L209 280L153 187L116 224L124 266L92 298L58 238L18 204L50 172L92 180L129 143L38 93Z"
-            stroke="#0f2a5f"
-            strokeWidth="22"
-            strokeLinejoin="miter"
-          />
-        </svg>
+          <div
+            style={{
+              display: "flex",
+              fontSize: "118px",
+              lineHeight: 0.82,
+              marginRight: "12px",
+            }}
+          >
+            Vocab
+          </div>
+          <div
+            style={{
+              display: "flex",
+              fontSize: "154px",
+              lineHeight: 0.82,
+            }}
+          >
+            Flow
+          </div>
+        </div>
       </div>
     ),
     {
